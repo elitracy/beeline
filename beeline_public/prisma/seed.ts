6684c6c205b5efebd88dccde7043091962fb28de
ETingRice <65531243+ETingRice@users.noreply.github.com>
2023-01-29 10:05:02 -0600

    logo on home

--- beeline_public/prisma/seed.ts
+++ beeline_public/prisma/seed.ts
@@ -17,94 +17,45 @@ async function seed() {
       name: "John Doe",
       airports: {
         create: [
-          { name: "IAH", city: "Houston", state: "TX", country: "USA" },
-          { name: "RNO", city: "Reno", state: "NV", country: "USA" },
-          { name: "SET", city: "Seattle", state: "WA", country: "USA" },
-          { name: "SFO", city: "San Fransisco", state: "CA", country: "USA" }
-        ]
-      },
-      airplanes: {
-        create: [
-          {
-            departure_time: new Date("2022-06-01T09:00:00.000Z"),
-            arrival_time: new Date("2022-06-01T12:00:00.000Z"),
-            airports: {
-              connect: [{ name: "IAH" }, { name: "RNO" }, { name: "SET" }, { name: "SFO" }]
-            },
-            seats: {
-              create: [
-                { seat_number: 1, seat_row: 1, seat_class: "economy" },
-                { seat_number: 2, seat_row: 1, seat_class: "economy" }
-              ]
-            }
-          },
-          {
-            departure_time: new Date("2022-06-01T09:00:00.000Z"),
-            arrival_time: new Date("2022-06-01T12:00:00.000Z"),
-            airports: {
-              connect: [{ name: "RNO" }, { name: "SET" }]
-            },
-            seats: {
-              create: [
-                { seat_number: 1, seat_row: 1, seat_class: "economy" },
-                { seat_number: 2, seat_row: 1, seat_class: "economy" }
-              ]
-            }
-          },
-          {
-            departure_time: new Date("2022-06-01T09:00:00.000Z"),
-            arrival_time: new Date("2022-06-01T12:00:00.000Z"),
-            airports: {
-              connect: [{ name: "SET" }, { name: "SFO" }]
-            },
-            seats: {
-              create: [
-                { seat_number: 1, seat_row: 1, seat_class: "economy" },
-                { seat_number: 2, seat_row: 1, seat_class: "economy" }
-              ]
-            }
-          }
-        ]
-      },
-
-    }
-  })
-
-  const passenger2 = await prisma.passenger.create({
-    data: {
-      email: "jane@example.com",
-      password: {
-        create: {
-          hash: await bcrypt.hash("password", 10)
-        }
-      },
-      name: "Jane Doe",
-      airports: {
-        create: [
           { name: "LAX", city: "Los Angeles", state: "CA", country: "USA" },
-          { name: "JFK", city: "New York", state: "NY", country: "USA" }
+          { name: "PHX", city: "Phoenix", state: "AZ", country: "USA" },
+          { name: "MSP", city: "Minneapolis", state: "MN", country: "USA" },
+          { name: "DFW", city: "Dallas", state: "TX", country: "USA" }
         ]
       },
       airplanes: {
         create: [
+    
           {
-            departure_time: new Date("2022-06-01T09:00:00.000Z"),
-            arrival_time: new Date("2022-06-01T12:00:00.000Z"),
+            departure_time: new Date("2023-01-28T08:38:00.000Z"),
+            arrival_time: new Date("2023-01-28T03:15:40.000Z"),
             airports: {
-              connect: [{ name: "LAX" }, { name: "JFK" }]
-            },
-            seats: {
-              create: [
-                { seat_number: 1, seat_row: 1, seat_class: "economy" },
-                { seat_number: 2, seat_row: 1, seat_class: "economy" }
-              ]
+              connect: [{ name: "LAX" }, { name: "PHX" }]
             }
+           
+          },
+          {
+            departure_time: new Date("2023-01-28T08:38:00.000Z"),
+            arrival_time: new Date("2023-01-28T05:20:00.000Z"),
+            airports: {
+              connect: [{ name: "PHX" }, { name: "MSP" }]
+            }
+           
+          },
+          {
+            departure_time: new Date("2023-01-28T07:55:17.142Z"),
+            arrival_time: new Date("2023-01-28T09:55:17.142Z"),
+            airports: {
+              connect: [{ name: "MSP" }, { name: "DFW" }]
+            }
+            
           }
         ]
       },
 
     }
   })
+
 
   console.log(`Database has been seeded. 🌱`);
 }
